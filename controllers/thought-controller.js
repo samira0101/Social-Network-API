901810fdf28868952c5e6f0f1115038a3620eda9
Allure <samira-h@hotmail.co.uk>
2022-05-07 16:37:45 +0100

    add and delete reaction function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -53,3 +53,31 @@ const thoughtController = {
             .catch(err => res.json(err));
     },
 
+    //add reaction
+    addReaction ({ params, body}, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought with this ID!' });
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
+
+    //delete Reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    },
+
