a9e8c436b5256fec781384e837e70cd12638fe35
Allure <samira-h@hotmail.co.uk>
2022-05-07 16:39:19 +0100

    create user and adding friend functions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -34,3 +34,28 @@ const userController = {
                res.status(500).json(err)
         });
      },
+
+     //create User
+     createUser({ body }, res) {
+         User.create(body)
+         .then(dbUserData => res.json(dbUserData))
+         .catch(err => res.status(400).json(err));
+     },
+
+     //add friend
+     addFriend({ params }, res) {
+         User.findOneAndUpdate(
+             {_id: params.userId},
+             { $push: { friends: params.friendId } },
+             { new: true, runValidators: true}
+         )
+         .then(dbUserData => {
+             if (!dbUserData) {
+                 res.status(404).json({ message: 'No user found with this ID!' });
+                 return;
+             }
+             res.json(dbUserData);
+         })
+         .catch(err => res.json(err));
+     },
+
